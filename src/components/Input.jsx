df97e1382604d2476ae7981e7a16962f178f7728
aknadif <64004224+aknadif@users.noreply.github.com>
2022-05-29 00:05:12 +0700

    Design UI

---
+++ src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Input(props) {
+  return (
+    <div className="relative z-0 w-3/4 mb-6 group">
+      <input
+        type={props.type}
+        name={props.name}
+        className="block py-2.5 px-0 w-full text-md bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        placeholder=" "
+        required
+      />
+      <label
+        htmlFor={props.name}
+        className="peer-focus:font-medium absolute text-md font-medium text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        {props.placeholder}
+      </label>
+    </div>
+  );
+}
+
+export default Input;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { SearchIcon } from "@heroicons/react/solid";
+function Navbar() {
+  return (
+    <nav className="bg-gray-900 p-4 border-b-2 border-gray-600">
+      <div className="container flex flex-col md:flex-row justify-between items-center ">
+        <a href="/" className="text-gray-200 font-semibold text-xl ">
+          Notes App
+        </a>
+        <form className="flex items-center py-4 md:py-0">
+          <label htmlFor="simple-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <SearchIcon className="w-5 h-5 text-gray-400" />
+            </div>
+            <input
+              type="text"
+              id="simple-search"
+              className="border text-sm rounded-lg block w-full pl-10 p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Search"
+              required
+            />
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
