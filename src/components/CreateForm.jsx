8b47e1f65a64719dfbbfc8b18483e59d033d5b88
aknadif <64004224+aknadif@users.noreply.github.com>
2022-05-29 07:33:08 +0700

    Penambahan fitur delete

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -2,18 +2,56 @@ import React from "react";
 import Input from "./Input";
 import InputTextArea from "./InputTextArea";
 import Button from "./Button";
-function CreateForm() {
-  return (
-    <form action="" className="border-2 border-gray-600 md:w-1/2 mx-auto rounded-lg relative">
-      <div className="flex flex-wrap justify-center py-5">
-        <Input type="text" name="input_title" placeholder="Title" />
-        <InputTextArea name="input_body" placeholder="Input body..." label="Body" />
-      </div>
-      <div className="text-center md:text-right p-0 -mt-4 pb-4 md:px-14 md:pb-6">
-        <Button name="Create" />
-      </div>
-    </form>
-  );
+class CreateForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "",
+      body: "",
+    };
+
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+  }
+
+  onNameChangeEventHandler(event) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        name: event.target.value,
+      };
+    });
+  }
+
+  onTagChangeEventHandler(event) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        tag: event.target.value,
+      };
+    });
+  }
+
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+    this.props.addContact(this.state);
+  }
+
+  render() {
+    return (
+      <form className="border-2 border-gray-600 md:w-1/2 mx-auto rounded-lg relative" onSubmit={this.onSubmitEventHandler}>
+        <div className="flex flex-wrap justify-center py-5">
+          <Input type="text" name="input_title" placeholder="Title" value={this.state.title} onChange={this.onNameChangeEventHandler} />
+          <InputTextArea name="input_body" placeholder="Input body..." label="Body" value={this.state.body} onChange={this.onTagChangeEventHandler}/>
+        </div>
+        <div className="text-center md:text-right p-0 -mt-4 pb-4 md:px-14 md:pb-6">
+          <Button name="Create" />
+        </div>
+      </form>
+    );
+  }
 }
 
 export default CreateForm;
