8e7f93e473dc2ab1e81c9d1f7f2fc12d228e6bbc
aknadif <64004224+aknadif@users.noreply.github.com>
2022-05-29 10:30:34 +0700

    Penambahan fitur limit karakter

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -9,6 +9,7 @@ class CreateForm extends React.Component {
     this.state = {
       title: "",
       body: "",
+      char: 50,
     };
 
     this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
@@ -20,7 +21,8 @@ class CreateForm extends React.Component {
     this.setState((prevState) => {
       return {
         ...prevState,
-        title: event.target.value,
+        title: event.target.value.substring(0, 49),
+        char: 50 - event.target.value.length,
       };
     });
   }
@@ -43,8 +45,8 @@ class CreateForm extends React.Component {
     return (
       <form className="border-2 border-gray-600 md:w-1/2 mx-auto rounded-lg relative" onSubmit={this.onSubmitEventHandler}>
         <div className="flex flex-wrap justify-center py-5">
-          <Input type="text" name="input_title" placeholder="Title" value={this.state.title} onChange={this.onTitleChangeEventHandler} />
-          <InputTextArea name="input_body" placeholder="Input body..." label="Body" value={this.state.body} onChange={this.onBodyChangeEventHandler}/>
+          <Input type="text" name="input_title" placeholder="Title" value={this.state.title} onChange={this.onTitleChangeEventHandler} char={this.state.char} />
+          <InputTextArea name="input_body" placeholder="Input body..." label="Body" value={this.state.body} onChange={this.onBodyChangeEventHandler} />
         </div>
         <div className="text-center md:text-right p-0 -mt-4 pb-4 md:px-14 md:pb-6">
           <Button name="Create" />
