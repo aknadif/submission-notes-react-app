70418bda7b58079ea4aa59bfc80d64d53aea7e05
aknadif <64004224+aknadif@users.noreply.github.com>
2022-05-29 07:51:53 +0700

    Penambahan fitur Create

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,6 +8,8 @@ function Input(props) {
         name={props.name}
         className="block py-2.5 px-0 w-full text-md bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
         placeholder=" "
+        value={props.value}
+        onChange={props.onChange}
         required
       />
       <label
